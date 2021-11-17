38b30f4b141c3f1407bf0bcaabf7a264ef5eb7c7
VikashShah15 <shah.vikas125@gmail.com>
2021-11-17 18:32:40 +0530

    Credentials Remove

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
-  BASE_URL_POST: 'https://82ekm3ohze.execute-api.ap-southeast-2.amazonaws.com/default/',
-  BASE_URL_GET: 'https://vs6xwddjgj.execute-api.ap-southeast-2.amazonaws.com/default/',
-  SEARVICE_PROVIDER: 'https://api.cared.com.au/searchProvider',
-  BASE_URL_PROFILE: 'https://planparserapi.cared.com.au/',
-  BASE_URL_AUTHENTICATION: 'https://backend-staging.cared.com.au/',
-  BASE_URL_AUTH_SERVER: 'https://backend-staging.cared.com.au/auth',
+  BASE_URL_POST: '',
+  BASE_URL_GET: '',
+  SEARVICE_PROVIDER: '',
+  BASE_URL_PROFILE: '',
+  BASE_URL_AUTHENTICATION: '',
+  BASE_URL_AUTH_SERVER: '',
   PARSING_TYPE: 'Text',
   isVisibleOnProduction: 1,
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,24 +4,24 @@
 export const environment = {
   production: false,
   TRACKING_ID: 'UA-197134759-1',
-  BASE_URL_POST: 'https://82ekm3ohze.execute-api.ap-southeast-2.amazonaws.com/default/',
-  BASE_URL_GET: 'https://vs6xwddjgj.execute-api.ap-southeast-2.amazonaws.com/default/',
-  SEARVICE_PROVIDER: 'https://api.cared.com.au/searchProvider',
-  BASE_URL_PROFILE: 'https://planparserapi.cared.com.au/',
-  BASE_URL_AUTHENTICATION: 'http://ec2-3-25-133-138.ap-southeast-2.compute.amazonaws.com:8085/',
-  // BASE_URL_AUTHENTICATION: 'https://backend-staging.cared.com.au/',
-  BASE_URL_AUTH_SERVER: 'http://ec2-3-25-133-138.ap-southeast-2.compute.amazonaws.com:8090/',
+  BASE_URL_POST: '',
+  BASE_URL_GET: '',
+  SEARVICE_PROVIDER: '',
+  BASE_URL_PROFILE: '',
+  BASE_URL_AUTHENTICATION: '',
+  // BASE_URL_AUTHENTICATION: '',
+  BASE_URL_AUTH_SERVER: '',
   PARSING_TYPE: 'Text',
-  // BASE_URL: 'https://d7yhsqqh92.execute-api.us-west-2.amazonaws.com/Test/',
+  // BASE_URL: '',
   isVisibleOnProduction: 1,
   firebaseConfig: {
-    apiKey: 'AIzaSyCBRX-cZ3lhUOy-Y35OVvG3SaiyO-fFR9Y',
-  authDomain: 'cared-portal-ed892.firebaseapp.com',
-  projectId: 'cared-portal-ed892',
-  storageBucket: 'cared-portal-ed892.appspot.com',
-  messagingSenderId: '733559276327',
-  appId: '1:733559276327:web:b39d4ade8f02886c8adf02',
-  measurementId: 'G-XM0KMB8TZ1'
+    apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+  measurementId: ''
   }
 };
 
